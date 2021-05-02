6f07da49e7e8d5e2d3ea4f49647456fd4d8f8bb5
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-02 13:08:02 +0700

    reusable coded

--- resources/views/admin/denda/index.blade.php
+++ resources/views/admin/denda/index.blade.php
@@ -1,78 +1,73 @@
 @extends('layouts.app', ['title' => 'Perpus | Denda'])
 
 @section('content')
-<div class="w-full overflow-hidden rounded-lg shadow-xs">
-  <div class="w-full overflow-x-auto">
-    <table class="w-full whitespace-no-wrap">
-      <thead>
-        <tr
-          class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
-        >
-          <th class="px-4 py-3">#</th>
-          <th class="px-4 py-3">Denda Sebesar</th>
-          <th class="px-4 py-3">Telat (Hari)</th>
-          <th class="px-4 py-3">Kode Peminjaman</th>
-          <th class="px-4 py-3">Nama Siswa</th>
-          <th class="px-4 py-3">Nama Buku</th>
-        </tr>
-      </thead>
-        @forelse ($borrowings as $borrowing)
-        <tbody
-        class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-        >
-        <tr class="text-gray-700 dark:text-gray-400">
-          <th class="px-4 py-3 text-sm">
-            {{ $loop->iteration + $borrowings->firstItem() - 1 . '.' }}
-            </th>
-            @php
-                $return = date_create($borrowing['return_date']);
-                $date = date_create(date('Y-m-d'));
-                $late = date_diff($return, $date);
-                $day = $late->format("%a");
-                // fine calculate
-                $fine = $day * 1000;
-            @endphp
-            <td class="px-4 py-3 text-sm">
-                {{ "Rp " . number_format($fine, 0,',','.') }}
-            </td>
-            <td class="px-4 py-3 text-sm">
-                <span
-                class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100"
-                >
-                    {{ $day . " Hari" }}
-                </span>
-            </td>
-            <td class="px-4 py-3 text-sm">
-                {{ $borrowing->borrow_code }}
-            </td>
-            <td class="px-4 py-3 text-sm">
-                {{ $borrowing->student->FullName }}
-            </td>
-            <td class="px-4 py-3 text-sm">
-                {{ $borrowing->book->name }}
-            </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="7" style="color: red; text-align: center; padding: 15px;">Data Denda Kosong!</td>
-            </tr>
-            </tbody>
-            @endforelse
-        </table>
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
+                <thead>
+                    <tr
+                        class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-4 py-3">#</th>
+                        <th class="px-4 py-3">Denda Sebesar</th>
+                        <th class="px-4 py-3">Telat (Hari)</th>
+                        <th class="px-4 py-3">Kode Peminjaman</th>
+                        <th class="px-4 py-3">Nama Siswa</th>
+                        <th class="px-4 py-3">Nama Buku</th>
+                    </tr>
+                </thead>
+                @forelse ($borrowings as $borrowing)
+                    <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                        <tr class="text-gray-700 dark:text-gray-400">
+                            <th class="px-4 py-3 text-sm">
+                                {{ $loop->iteration + $borrowings->firstItem() - 1 . '.' }}
+                            </th>
+                            @php
+                                $return = date_create($borrowing['return_date']);
+                                $date = date_create(date('Y-m-d'));
+                                $late = date_diff($return, $date);
+                                $day = $late->format('%a');
+                                // fine calculate
+                                $fine = $day * 1000;
+                            @endphp
+                            <td class="px-4 py-3 text-sm">
+                                {{ 'Rp ' . number_format($fine, 0, ',', '.') }}
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                <span
+                                    class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100">
+                                    {{ $day . ' Hari' }}
+                                </span>
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $borrowing->borrow_code }}
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $borrowing->student->FullName }}
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $borrowing->book->name }}
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="7" style="color: red; text-align: center; padding: 15px;">Data Denda Kosong!</td>
+                        </tr>
+                    </tbody>
+                @endforelse
+            </table>
+        </div>
+        <div
+            class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+            <!-- Pagination -->
+            <span class="flex col-span-4 mt-2">
+                <nav aria-label="Table navigation">
+                    <ul class="inline-flex items-center">
+                        <li>
+                            {{ $borrowings->links() }}
+                        </li>
+                    </ul>
+                </nav>
+            </span>
+        </div>
     </div>
-  <div
-    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
-  >
-    <!-- Pagination -->
-    <span class="flex col-span-4 mt-2">
-      <nav aria-label="Table navigation">
-        <ul class="inline-flex items-center">
-          <li>
-            {{ $borrowings->links() }}
-          </li>
-        </ul>
-      </nav>
-    </span>
-  </div>
-</div>
 @endsection
--- resources/views/admin/pengguna/index.blade.php
+++ resources/views/admin/pengguna/index.blade.php
@@ -1,92 +1,82 @@
 @extends('layouts.app', ['title' => 'Perpus | Pengguna'])
 
 @section('content')
-<a href="{{ route('admin.pengguna.create') }}" class="flex items-center justify-between px-2 py-2 mb-2 text-sm font-semibold leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red" style="width: 150px; background-color: rgb(29, 185, 29);">
-    <i class="fa fa-plus"></i>
-    <span style="font-size: 13px;">Tambah Pengguna</span>
-</a>
-<div class="w-full overflow-hidden rounded-lg shadow-xs">
-  <div class="w-full overflow-x-auto">
-    <table class="w-full whitespace-no-wrap">
-      <thead>
-        <tr
-          class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
-        >
-          <th class="px-4 py-3">#</th>
-          <th class="px-4 py-3">Level</th>
-          <th class="px-4 py-3">Nama</th>
-          <th class="px-4 py-3">Username</th>
-          <th class="px-4 py-3">Password</th>
-          <th class="px-4 py-3">Aksi</th>
-        </tr>
-      </thead>
-        @foreach ($users as $user)
-        <tbody
-        class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-        >
-        <tr class="text-gray-700 dark:text-gray-400">
-          <th class="px-4 py-3 text-sm">
-            {{ $loop->iteration + $users->firstItem() - 1 . '.'}}
-            </th>
-            <td class="px-4 py-3 text-xs">
-              <span
-                class="px-2 py-1 font-semibold leading-tight {{ $user->role == 'admin' ? 'text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100' : 'text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100' }}"
-              >
-                {{ $user->role }}
-              </span>
-            </td>
-            <td class="px-4 py-3 text-sm">
-                {{ $user->name }}
-            </td>
-            <td class="px-4 py-3 text-sm">
-                {{ $user->username }}
-            </td>
-            <td class="px-4 py-3 text-sm">
-                <b>rahasia</b>
-            </td>
-            <td class="px-4 py-3">
-                <div class="flex items-center space-x-4 text-sm">
-                    <form action="{{ route('admin.pengguna.destroy', $user->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button
-                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                        aria-label="Delete" onclick="return confirm('Apakah Anda yakin untuk menghapus data?');"
-                            >
-                                <svg
-                                    class="w-5 h-5"
-                                    aria-hidden="true"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                    fill-rule="evenodd"
-                                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                    clip-rule="evenodd"
-                                    ></path>
-                                </svg>
-                        </button>
-                    </form>
-                </div>
-            </td>
-            </tr>
-            </tbody>
-            @endforeach
-        </table>
+    <a href="{{ route('admin.pengguna.create') }}"
+        class="flex items-center justify-between px-2 py-2 mb-2 text-sm font-semibold leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red"
+        style="width: 150px; background-color: rgb(29, 185, 29);">
+        <i class="fa fa-plus"></i>
+        <span style="font-size: 13px;">Tambah Pengguna</span>
+    </a>
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
+                <thead>
+                    <tr
+                        class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-4 py-3">#</th>
+                        <th class="px-4 py-3">Level</th>
+                        <th class="px-4 py-3">Nama</th>
+                        <th class="px-4 py-3">Username</th>
+                        <th class="px-4 py-3">Password</th>
+                        <th class="px-4 py-3">Aksi</th>
+                    </tr>
+                </thead>
+                @foreach ($users as $user)
+                    <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                        <tr class="text-gray-700 dark:text-gray-400">
+                            <th class="px-4 py-3 text-sm">
+                                {{ $loop->iteration + $users->firstItem() - 1 . '.' }}
+                            </th>
+                            <td class="px-4 py-3 text-xs">
+                                <span
+                                    class="px-2 py-1 font-semibold leading-tight {{ $user->role == 'admin' ? 'text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100' : 'text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100' }}">
+                                    {{ $user->role }}
+                                </span>
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $user->name }}
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $user->username }}
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                <b>rahasia</b>
+                            </td>
+                            <td class="px-4 py-3">
+                                <div class="flex items-center space-x-4 text-sm">
+                                    <form action="{{ route('admin.pengguna.destroy', $user->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button
+                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                            aria-label="Delete"
+                                            onclick="return confirm('Apakah Anda yakin untuk menghapus data?');">
+                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+        </div>
+        <div
+            class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+            <!-- Pagination -->
+            <span class="flex col-span-4 mt-2">
+                <nav aria-label="Table navigation">
+                    <ul class="inline-flex items-center">
+                        <li>
+                            {{ $users->links() }}
+                        </li>
+                    </ul>
+                </nav>
+            </span>
+        </div>
     </div>
-  <div
-    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
-  >
-    <!-- Pagination -->
-    <span class="flex col-span-4 mt-2">
-      <nav aria-label="Table navigation">
-        <ul class="inline-flex items-center">
-          <li>
-            {{ $users->links() }}
-          </li>
-        </ul>
-      </nav>
-    </span>
-  </div>
-</div>
 @endsection

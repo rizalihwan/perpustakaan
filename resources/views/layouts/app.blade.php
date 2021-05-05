7c4e663f55f5aa577b574bd7498a980044e65a8b
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-05 18:57:50 +0700

    added swal cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,55 +1,44 @@
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $title ?? 'Perpus' }}</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
     <!-- Icon Web -->
     <link rel="shortcut icon" href="{{ asset('logo/logoweb.png') }}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/tailwind.output.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-    <script
-      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
-      defer
-    ></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="{{ asset('assets/js/init-alpine.js') }}"></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
-    />
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
-      defer
-    ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
     <script src="{{ asset('assets/js/charts-lines.js') }}" defer></script>
     <script src="{{ asset('assets/js/charts-pie.js') }}" defer></script>
-  </head>
-  <body>
+</head>
+
+<body>
     @auth
-        <div
-        class="flex h-screen bg-gray-50 dark:bg-gray-900"
-        :class="{ 'overflow-hidden': isSideMenuOpen }"
-        >
-        <!-- Desktop sidebar -->
+        <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+            <!-- Desktop sidebar -->
             <x-sidebar></x-sidebar>
-        <!-- Mobile sidebar -->
+            <!-- Mobile sidebar -->
             <x-mobilesidebar></x-mobilesidebar>
             <div class="flex flex-col flex-1 w-full">
                 <!-- Header -->
-                    <x-navbar></x-navbar>
-                    <main class="h-full overflow-y-auto">
-                        <div class="container px-6 mx-auto grid">
-                            <!-- Breadcrumb -->
-                                <x-breadcrumb></x-breadcrumb>
-                            <!-- Main Content -->
-                            @yield('content')
-                        </div>
-                    </main>
+                <x-navbar></x-navbar>
+                <main class="h-full overflow-y-auto">
+                    <div class="container px-6 mx-auto grid">
+                        <!-- Breadcrumb -->
+                        <x-breadcrumb></x-breadcrumb>
+                        <!-- Main Content -->
+                        @yield('content')
+                    </div>
+                </main>
             </div>
         </div>
     @else
@@ -58,6 +47,8 @@
     @yield('script')
     <!-- Swal Vendor -->
     @include('sweetalert::alert')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     @yield('script')
-  </body>
+</body>
+
 </html>

960d8484ac171104b8e87a28d1785ddce272d836
Dreamy <55319714+UltraInstinct46@users.noreply.github.com>
2022-10-10 12:39:52 +0700

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,6 +16,8 @@ class Authenticate extends Middleware
     {
         if (! $request->expectsJson()) {
             return route('login');
+        }else if(! $requets->expectsJson()){
+            return route('login');
         }
     }
 }
